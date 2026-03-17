86d74eacd9de53eb593381efaee8b29b1c477dbd
mohmadimran <pathanimrankhan308@gmail.com>
2026-03-17 13:27:14 +0530

    fix the base url issue

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://militry-assets-managment.onrender.com/',
+  baseURL: 'https://kristalball-backend-014q.onrender.com/',
 });
 
 API.interceptors.request.use(
